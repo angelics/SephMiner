b7ba930910e7844c6b605097fa996c19c5ff2164
angelics <josywong@hotmail.com>
2018-06-24 15:42:34 +0800

    dumax 092
    dumax 092

---
+++ Miners/CcminerDumax-092.ps1
@@ -0,0 +1,50 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+$Type = "NVIDIA"
+if (-not $Devices.$Type) {return} # No NVIDIA mining device present in system
+
+$DriverVersion = (Get-Devices).NVIDIA.Platform.Version -replace ".*CUDA ",""
+$RequiredVersion = "9.1.00"
+if ($DriverVersion -lt $RequiredVersion) {
+    Write-Log -Level Warn "Miner ($($Name)) requires CUDA version $($RequiredVersion) or above (installed version is $($DriverVersion)). Please update your Nvidia drivers to 390.77 or newer. "
+    return
+}
+
+$Path = ".\Bin\NVIDIA-Dumax-092\ccminer.exe"
+$Uri = "https://github.com/DumaxFr/ccminer/releases/download/dumax-0.9.2/ccminer-dumax-0.9.2-win64.zip"
+$Fee = 0
+
+$Commands = [PSCustomObject]@{
+    "phi"  = " -N 1" #phi
+    "phi2" = "" #phi2
+    "x16r" = " -N 3" #X16r
+    "x16s" = " -N 3" #X16s
+    "x17"  = " -N 1" #X17
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+
+    $HashRate = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * (1 - $Fee / 100)
+
+    [PSCustomObject]@{
+        Type      = $Type
+        Path      = $Path
+        Arguments = "-a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_) --submit-stale"
+        HashRates = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+        API       = "Ccminer"
+        Port      = 4068
+        URI       = $Uri
+        MinerFee  = @($Fee)
+    }
+}
