30036ceb716fc76c0b9c2a8747537c2819e30eec
angelics <josywong@hotmail.com>
2018-07-10 19:20:23 +0800

    update zenemy112a
    update zenemy112a

---
+++ Miners/NVIDIA-ZEnemy-112a.ps1
@@ -0,0 +1,88 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+if (-not $Devices.NVIDIA) {return} # No NVIDIA mining device present in system
+
+$DriverVersion = (Get-Devices).NVIDIA.Platform.Version -replace ".*CUDA ",""
+$RequiredVersion = "9.2.00"
+if ($DriverVersion -lt $RequiredVersion) {
+    Write-Log -Level Warn "Miner ($($Name)) requires CUDA version $($RequiredVersion) or above (installed version is $($DriverVersion)). Please update your Nvidia drivers to 397.93 or newer. "
+    return
+}
+
+$Type = "NVIDIA"
+$Path = ".\Bin\NVIDIA-ZEnemy-112a\z-enemy.exe"
+$API  = "Ccminer"
+$Uri  = "http://semitest.000webhostapp.com/binary/z-enemy.1-12-cuda9.2.zip"
+$Port = Get-FreeTcpPort -DefaultPort 4068
+$Fee  = 1
+
+$Commands = [PSCustomObject]@{
+    "aeriumx"    = "" #aeriumx
+    "bitcore"    = "" #Bitcore
+    "c11"        = "" #c11 NVIDIA-Alexis78-12b1
+    "phi"        = "" #Phi CcminerDumax-093
+    "phi2"       = "" #Phi2
+    "poly"       = "" #poly
+    "vit"        = "" #Vitalium
+    "skunk"      = "" #skunk
+    "sonoa"      = "" #sonoa
+    "timetravel" = "" #timetravel
+    "tribus"     = "" #Tribus
+    "x16s"       = " -i 21" #Pigeon
+    "x16r"       = " -i 21" #Raven
+    "x17"        = "" #X17 NVIDIA-Alexis78-12b1
+    "xevan"      = "" #Xevan
+}
+
+$CommonCommands = "" #eg. " -d 0,1,8,9"
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+
+    Switch ($Algorithm_Norm) {
+        "allium"        {$ExtendInterval = 2}
+        "CryptoNightV7" {$ExtendInterval = 2}
+        "Lyra2RE2"      {$ExtendInterval = 2}
+        "phi"           {$ExtendInterval = 2}
+        "phi2"          {$ExtendInterval = 2}
+        "tribus"        {$ExtendInterval = 2}
+        "X16R"          {$ExtendInterval = 3}
+        "X16S"          {$ExtendInterval = 3}
+        "X17"           {$ExtendInterval = 2}
+        "Xevan"         {$ExtendInterval = 2}
+        default         {$ExtendInterval = 0}
+    }
+	
+    Switch ($Algorithm_Norm) {
+        "Lyra2RE2" {$Average = 1}
+        "lyra2z"   {$Average = 1}
+        "phi"      {$Average = 1}
+        "tribus"   {$Average = 1}
+        "Xevan"    {$Average = 1}
+        default    {$Average = 3}
+    }
+
+    $HashRate = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * (1 - $Fee / 100)
+
+    [PSCustomObject]@{
+        Type           = $Type
+        Path           = $Path
+        Arguments      = "-q -b $($Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_)$($CommonCommands) -N $($Average)"
+        HashRates      = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+        API            = $API
+        Port           = $Port
+        URI            = $Uri
+        MinerFee       = @($Fee)
+        ExtendInterval = $ExtendInterval
+    }
+}
