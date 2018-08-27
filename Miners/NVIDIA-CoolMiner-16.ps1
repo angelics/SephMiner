963b8ad050bceb0ce610854466a11febd56ac4fa
angelics <josywong@hotmail.com>
2018-08-27 12:43:27 +0800

    coolminer 16

---
+++ Miners/NVIDIA-CoolMiner-16.ps1
@@ -0,0 +1,78 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+if (-not $Devices.NVIDIA) {return} # No NVIDIA mining device present in system
+
+$DriverVersion = (Get-Devices).NVIDIA.Platform.Version -replace ".*CUDA ",""
+$RequiredVersion = "9.2.00"
+if ($DriverVersion -lt $RequiredVersion) {
+    Write-Log -Level Warn "Miner ($($Name)) requires CUDA version $($RequiredVersion) or above (installed version is $($DriverVersion)). Please update your Nvidia drivers to 397.93 or newer. "
+    return
+}
+
+$Type = "NVIDIA"
+$Path = ".\Bin\NVIDIA-CoolMiner-16\coolMiner-x64.exe"
+$API  = "Ccminer"
+$Uri  = "http://semitest.000webhostapp.com/binary/coolMiner-x64-v1-6.7z"
+$Port = Get-FreeTcpPort -DefaultPort 4068
+$Fee  = 1
+
+$Commands = [PSCustomObject]@{
+    "lyra2z" = "" #Lyra2z
+}
+
+$CommonCommands = "" #eg. " -d 0,1,8,9"
+
+$DeviceIDs = (Get-DeviceIDs -Config $Config -Devices $Devices -Type NVIDIA -DeviceTypeModel $($Devices.NVIDIA) -DeviceIdBase 10 -DeviceIdOffset 0)."$(if ($Type -EQ "NVIDIA"){"All"}else{$Type})"
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+
+    Switch ($Algorithm_Norm) {
+        "allium"        {$ExtendInterval = 2}
+        "CryptoNightV7" {$ExtendInterval = 2}
+        "Lyra2RE2"      {$ExtendInterval = 2}
+        "phi"           {$ExtendInterval = 2}
+        "phi2"          {$ExtendInterval = 2}
+        "tribus"        {$ExtendInterval = 2}
+        "X16R"          {$ExtendInterval = 3}
+        "X16S"          {$ExtendInterval = 3}
+        "X17"           {$ExtendInterval = 2}
+        "Xevan"         {$ExtendInterval = 2}
+        default         {$ExtendInterval = 0}
+    }
+	
+    Switch ($Algorithm_Norm) {
+        "Lyra2RE2" {$Average = 1}
+        "lyra2z"   {$Average = 1}
+        "phi"      {$Average = 1}
+        "tribus"   {$Average = 1}
+        "Xevan"    {$Average = 1}
+        default    {$Average = 3}
+    }
+	
+    if ($Pools.$Algorithm_Norm) {
+	
+        $HashRate = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * (1 - $Fee / 100)
+
+        [PSCustomObject]@{
+            Type      = $Type
+            Path      = $Path
+            Arguments = "-q -b $($Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_)$($CommonCommands) -N $($Average) -d $($DeviceIDs -join ',')"
+            HashRates = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+            API       = $APi
+            Port      = $Port
+            URI       = $Uri
+            MinerFee  = @($Fee)
+        }
+    }
+}
