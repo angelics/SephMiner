ecd75e1023305c9e60c7302d81afa7d2eb8de1c3
angelics <josywong@hotmail.com>
2018-10-12 19:45:14 +0800

    klaust 823
    klaust 823

---
+++ Miners/Disable/NVIDIA-Klaust-823.ps1
@@ -0,0 +1,55 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+if (-not $Devices.NVIDIA) {return} # No NVIDIA mining device present in system
+
+$DriverVersion = (Get-Devices).NVIDIA.Platform.Version -replace ".*CUDA ",""
+$RequiredVersion = "10.0.00"
+if ($DriverVersion -lt $RequiredVersion) {
+    Write-Log -Level Warn "Miner ($($Name)) requires CUDA version $($RequiredVersion) or above (installed version is $($DriverVersion)). Please update your Nvidia drivers to 390.77 or newer. "
+    return
+}
+
+$Type = "NVIDIA"
+$Path = ".\Bin\NVIDIA-KlausT-823\ccminer.exe"
+$API  = "Ccminer"
+$Uri  = "https://github.com/KlausT/ccminer/releases/download/8.23/ccminer-823-cuda10-x64.zip"
+$Port = Get-FreeTcpPort -DefaultPort 4068
+$Fee  = 0
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{Algorithm = "lyra2v2"; Params = ""; Zpool = ""; MiningPoolHubCoins = ""} #lyra2v2
+    [PSCustomObject]@{Algorithm = "skein"; Params = ""; Zpool = ""; MiningPoolHubCoins = ""} #skein
+)
+
+$CommonCommands = "" #eg. " -d 0,1,8,9"
+
+$DeviceIDs = (Get-DeviceIDs -Config $Config -Devices $Devices -Type NVIDIA -DeviceTypeModel $($Devices.NVIDIA) -DeviceIdBase 10 -DeviceIdOffset 0)."$(if ($Type -EQ "NVIDIA"){"All"}else{$Type})"
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Where-Object {$Pools.(Get-Algorithm $_.Algorithm).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_.Algorithm
+	
+    $StaticDiff = $_."$($Pools.$Algorithm_Norm.Name)"
+
+    $HashRate = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * (1 - $Fee / 100)
+
+    [PSCustomObject]@{
+        Type      = $Type
+        Path      = $Path
+        Arguments = "-q -b $($Port) -a $($_.Algorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($StaticDiff)$($_.Params)$($CommonCommands) -d $($DeviceIDs -join ',')"
+        HashRates = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+        API       = $API
+        Port      = $Port
+        URI       = $Uri
+        MinerFee  = @($Fee)
+    }
+}
