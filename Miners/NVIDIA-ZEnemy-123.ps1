0ce731a0ccf938a6181b5943c96176358a4d866a
angelics <josywong@hotmail.com>
2018-11-03 14:01:26 +0800

    zenemy 123
    zenemy 123

--- Miners/NVIDIA-ZEnemy-123.ps1
+++ Miners/NVIDIA-ZEnemy-123.ps1
@@ -10,35 +10,35 @@ param(
 if (-not $Devices.NVIDIA) {return} # No NVIDIA mining device present in system
 
 $Type = "NVIDIA"
-$Path = ".\Bin\NVIDIA-TRex-072\t-rex.exe"
+$Path = ".\Bin\NVIDIA-ZEnemy-123\z-enemy.exe"
 $API  = "Ccminer"
-$Uri  = "http://semitest.000webhostapp.com/binary/t-rex-0.7.2-win-cuda10.0.zip"
+$Uri  = "http://semitest.000webhostapp.com/binary/z-enemy.1-23-cuda10.0_x64.zip"
 $Port = Get-FreeTcpPort -DefaultPort 4068
 $Fee  = 1
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{Algorithm = "balloon"; Params = ""} #balloon
-    [PSCustomObject]@{Algorithm = "bcd"; Params = ""; Zpool = ""} #bcd
-    [PSCustomObject]@{Algorithm = "bitcore"; Params = ""; Zpool = ""} #bitcore
-    #[PSCustomObject]@{Algorithm = "c11"; Params = ""; Zpool = ""} #c11 NVIDIA-ZEnemy-122
+    [PSCustomObject]@{Algorithm = "aeriumx"; Params = ""; Zpool = ""} #aeriumx
+    #[PSCustomObject]@{Algorithm = "bcd"; Params = ""; Zpool = "";} #bcd NVIDIA-TRex-072
+    #[PSCustomObject]@{Algorithm = "bitcore"; Params = ""; Zpool = ""} #Bitcore NVIDIA-TRex-072
+    [PSCustomObject]@{Algorithm = "c11"; Params = ""; Zpool = ""} #c11
+    [PSCustomObject]@{Algorithm = "hex"; Params = ""; Zpool = ""} #HEX
     [PSCustomObject]@{Algorithm = "hsr"; Params = ""; Zpool = ""} #hsr
-    [PSCustomObject]@{Algorithm = "hmq1725"; Params = ""; Zpool = ""} #hmq1725
-    #[PSCustomObject]@{Algorithm = "lyra2z"; Params = ""; Zpool = ""} #lyra2z NVIDIA-CryptoDredge-092
-    #[PSCustomObject]@{Algorithm = "polytimos"; Params = ""; Zpool = ""} #polytimos NVIDIA-CryptoDredge-095
-    #[PSCustomObject]@{Algorithm = "phi"; Params = ""; Zpool = ""} #phi NVIDIA-CryptoDredge-095
+    #[PSCustomObject]@{Algorithm = "phi"; Params = ""; Zpool = ""} #Phi NVIDIA-CryptoDredge-095
+    #[PSCustomObject]@{Algorithm = "phi2"; Params = ""; Zpool = ""} #Phi2 NVIDIA-CryptoDredge-095
+    #[PSCustomObject]@{Algorithm = "poly"; Params = ""; Zpool = ""} #poly NVIDIA-CryptoDredge-095
+    [PSCustomObject]@{Algorithm = "vit"; Params = ""; Zpool = ""} #Vitalium
     [PSCustomObject]@{Algorithm = "renesis"; Params = ""} #renesis
     [PSCustomObject]@{Algorithm = "skunk"; Params = ""; Zpool = ""} #skunk
-    [PSCustomObject]@{Algorithm = "sha256t"; Params = ""} #sha256t
     [PSCustomObject]@{Algorithm = "sonoa"; Params = ""} #sonoa
-    [PSCustomObject]@{Algorithm = "timetravel"; Params = ""} #timetravel
-    #[PSCustomObject]@{Algorithm = "tribus"; Params = ""; Zpool = ""} #tribus NVIDIA-ZEnemy-122
-    [PSCustomObject]@{Algorithm = "x16r"; Params = ""; Zpool = ""} #x16r
-    [PSCustomObject]@{Algorithm = "x16s"; Params = ""; Zpool = ""} #x16s
-    [PSCustomObject]@{Algorithm = "x22i"; Params = ""; Zpool = ""} #x22i
-    #[PSCustomObject]@{Algorithm = "x17"; Params = ""; Zpool = ""} #x17 NVIDIA-ZEnemy-123
+    #[PSCustomObject]@{Algorithm = "timetravel"; Params = ""; Zpool = ""} #timetravel NVIDIA-TRex-072
+    [PSCustomObject]@{Algorithm = "tribus"; Params = ""; Zpool = ""} #Tribus
+    [PSCustomObject]@{Algorithm = "x16s"; Params = " -i 21"; Zpool = ""} #x16s
+    [PSCustomObject]@{Algorithm = "x16r"; Params = " -i 21"; Zpool = ""} #x16r
+    [PSCustomObject]@{Algorithm = "x17"; Params = ""; Zpool = ""} #x17
+    [PSCustomObject]@{Algorithm = "xevan"; Params = ""; Zpool = ""} #xevan
 )
 
-$CommonCommands = " -N 60" #eg. " -d 0,1,8,9"
+$CommonCommands = "" #eg. " --cpu-affinity=0x3" core0,1
 
 $DeviceIDs = (Get-DeviceIDs -Config $Config -Devices $Devices -Type NVIDIA -DeviceTypeModel $($Devices.NVIDIA) -DeviceIdBase 10 -DeviceIdOffset 0)."$(if ($Type -EQ "NVIDIA"){"All"}else{$Type})"
 
@@ -47,9 +47,9 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Commands | Where-Object {$Pools.(Get-Algorithm $_.Algorithm).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
 
     $Algorithm_Norm = Get-Algorithm $_.Algorithm
-	
-    $StaticDiff = $_."$($Pools.$Algorithm_Norm.Name)"
 
+    $StaticDiff = $_."$($Pools.$Algorithm_Norm.Name)"
+	
     Switch ($Algorithm_Norm) {
         "allium"        {$ExtendInterval = 2}
         "CryptoNightV7" {$ExtendInterval = 2}
@@ -65,12 +65,21 @@ $Commands | Where-Object {$Pools.(Get-Algorithm $_.Algorithm).Protocol -eq "stra
         default         {$ExtendInterval = 0}
     }
 	
+    Switch ($Algorithm_Norm) {
+        "Lyra2RE2" {$Average = 1}
+        "lyra2z"   {$Average = 1}
+        "phi"      {$Average = 1}
+        "tribus"   {$Average = 1}
+        "Xevan"    {$Average = 1}
+        default    {$Average = 3}
+    }
+
     $HashRate = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * (1 - $Fee / 100)
 
     [PSCustomObject]@{
         Type           = $Type
         Path           = $Path
-        Arguments      = "-b 127.0.0.1:$($Port) --no-color --quiet -a $($_.Algorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($StaticDiff)$($_.Params)$($CommonCommands) -d $($DeviceIDs -join ',')"
+        Arguments      = "-q -b $($Port) -a $($_.Algorithm) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($StaticDiff)$($_.Params)$($CommonCommands) -N $($Average) -d $($DeviceIDs -join ',')"
         HashRates      = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
         API            = $API
         Port           = $Port
