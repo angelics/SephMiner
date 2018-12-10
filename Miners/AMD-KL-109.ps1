ea54419f35c1b519479847f18d8c71faf23e106f
angelics <josywong@hotmail.com>
2018-12-10 19:01:17 +0800

    update
    kl 109 untested
    zenemy 126

---
+++ Miners/AMD-KL-109.ps1
@@ -0,0 +1,68 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+if (-not $Devices.AMD) {return} # No AMD mining device present in system
+
+$Type = "AMD"
+$Path = ".\Bin\AMD-KL-109\sgminer.exe"
+$API  = "Xgminer"
+$Uri  = "https://github.com/KL0nLutiy/sgminer-kl/releases/download/kl-1.0.9/sgminer-kl-1.0.9-windows.zip"
+$Port = Get-FreeTcpPort -DefaultPort 4028
+$Fee  = 1
+
+$Commands = [PSCustomObject]@{
+    "aergo" = " -X 256 -g 2" #aergo
+    "c11"   = " -X 256 -w 256 -g 2" #c11
+    "geek"   = " -X 256 -g 2" #geek
+    "phi"   = " -X 256 -w 256 -g 2" #phi
+    "polytimos"   = " -X 256 -w 256 -g 2" #polytimos
+    "skunk"  = " -X 256 -w 256 -g 2" #skunk
+    "tribus"  = " -X 256 -g 2" #tribus
+    "x16r"  = " -X 256 -g 2 --intensity 18" #Raven increase 19,21
+    "x16s"  = " -X 256 -g 2" #x16s
+    "x17"   = " -X 256 -g 2" #x17
+    "xevan" = " -X 256 -g 2" #Xevan
+}
+
+$CommonCommands = "" #eg. " -d 0,1,8,9"
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+
+    Switch ($Algorithm_Norm) {
+        "allium"        {$ExtendInterval = 2}
+        "CryptoNightV7" {$ExtendInterval = 2}
+        "Lyra2RE2"      {$ExtendInterval = 2}
+        "phi"           {$ExtendInterval = 2}
+        "phi2"          {$ExtendInterval = 2}
+        "tribus"        {$ExtendInterval = 2}
+        "X16R"          {$ExtendInterval = 3}
+        "X16S"          {$ExtendInterval = 3}
+        "X17"           {$ExtendInterval = 2}
+        "Xevan"         {$ExtendInterval = 2}
+        default         {$ExtendInterval = 0}
+    }
+
+    $HashRate = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * (1 - $Fee / 100)
+
+    [PSCustomObject]@{
+        Type           = $Type
+        Path           = $Path
+        Arguments      = "--api-listen --api-port $($Port) -k $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_)$($CommonCommands) --gpu-platform $([array]::IndexOf(([OpenCl.Platform]::GetPlatformIDs() | Select-Object -ExpandProperty Vendor), 'Advanced Micro Devices, Inc.'))"
+        HashRates      = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+        API            = $API
+        Port           = $Port
+        URI            = $Uri
+        MinerFee       = @($Fee)
+        ExtendInterval = $ExtendInterval
+    }
+}
